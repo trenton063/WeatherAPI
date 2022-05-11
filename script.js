dd1d858f8b36bb7e47b5e79cbee85e53569bb793
Trenton <050trenton@gmail.com>
2022-05-11 13:59:59 -0500

    getting location

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 let mainTemp = document.querySelector(".temp");
 
-
-const lat = 37.0902;
-const lng = 95.7129;
-
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
 const weatherAPI = function () {
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=51550f6ee61d0be1cff9a205feeaccbb`
-  )
+  getPosition()
+    .then((pos) => {
+      const { latitude: lat, longitude: lng } = pos.coords;
+      return fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=51550f6ee61d0be1cff9a205feeaccbb`
+      );
+    })
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
       mainTemp.innerHTML = Math.round(data.main.temp - 273.15) * 1.8 + 32;
     });
 };
+
 weatherAPI();
