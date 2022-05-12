afcba0369d647c7e634bc7af7e63f1d65923f7ea
Trenton <050trenton@gmail.com>
2022-05-12 15:26:13 -0500

    starting to add images

--- script.js
+++ script.js
@@ -5,7 +5,26 @@ let maxTemp = document.querySelector(".temp-max");
 // let sunrise = document.querySelector(".sunrise");
 // let sunset = document.querySelector(".sunset");
 let weatherDescription = document.querySelector(".weather-description");
-let humidity = document.querySelector('.humidity');
+let humidity = document.querySelector(".humidity");
+
+let currentImg;
+const imgContainer = document.querySelector(".img");
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement("img");
+    img.src = imgPath;
+
+    img.addEventListener("load", function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener("error", function () {
+      reject(new Error("Image not found"));
+    });
+  });
+};
 
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
@@ -24,12 +43,24 @@ const weatherAPI = function () {
     .then((data) => {
       console.log(data);
       mainTemp.textContent = Math.round((data.main.temp - 273.15) * 1.8 + 32);
-      feelsLike.textContent = Math.round((data.main.feels_like - 273.15) * 1.8 + 32);
-      minTemp.textContent = Math.round((data.main.temp_min - 273.15) * 1.8 + 32);
-      maxTemp.textContent = Math.round((data.main.temp_max - 273.15) * 1.8 + 32);
-      weatherDescription.textContent = (data.weather[0].description);
-      humidity.textContent = (`${data.main.humidity}%`);
-    });
+      feelsLike.textContent = Math.round(
+        (data.main.feels_like - 273.15) * 1.8 + 32
+      );
+      minTemp.textContent = Math.round(
+        (data.main.temp_min - 273.15) * 1.8 + 32
+      );
+      maxTemp.textContent = Math.round(
+        (data.main.temp_max - 273.15) * 1.8 + 32
+      );
+      weatherDescription.textContent = data.weather[0].description;
+      humidity.textContent = `${data.main.humidity}%`;
+      if (data.weather[0].description === "clear sky") {
+        let img = createImage("img/img-1.jpg");
+      }
+    })
+  // .then((test) => {
+  //   console.log('g');
+  // })
 };
 
 weatherAPI();
